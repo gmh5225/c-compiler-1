582f33f7d0687ab75122c9ad03beaad6e5f7669a
ifq7vj <ifq7vj9@gmail.com>
2023-09-03 23:19:07 +0000

    Add parser subfunctions

--- main.c
+++ main.c
@@ -149,6 +149,25 @@ struct Node {
     Node *rhs;
 };
 
+Node *new_node(NodeKind kind) {
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = kind;
+    return node;
+}
+
+Node *new_binary(NodeKind kind, Node *lhs, Node *rhs) {
+    Node *node = new_node(kind);
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+
+Node *new_num(long long val) {
+    Node *node = new_node(ND_NUM);
+    node->val = val;
+    return node;
+}
+
 //
 // Main
 //
