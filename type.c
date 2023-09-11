14852b448025663b3864d797356c9ef61002a99f
ifq7vj <ifq7vj9@gmail.com>
2023-09-11 04:59:57 +0000

    Add type checking

--- type.c
+++ type.c
@@ -46,16 +46,17 @@ void add_type(Node *node) {
     case ND_NE:
     case ND_LT:
     case ND_LE:
-    case ND_VAR:
     case ND_NUM:
         node->ty = ty_int;
         return;
+    case ND_VAR:
+        node->ty = node->var->ty;
+        return;
     case ND_DEREF:
-        if (node->lhs->ty->kind == TY_PTR) {
-            node->ty = node->lhs->ty->base;
-        } else {
-            node->ty = ty_int;
+        if (node->lhs->ty->kind != TY_PTR) {
+            error_tk(node->tk, "Invalid pointer dereference");
         }
+        node->ty = node->lhs->ty->base;
         return;
     case ND_ADDR:
         node->ty = pointer_to(node->lhs->ty);
